ecf23458d3010c0e55cf74d4bd1236bfc6f23be7
Satwik Raj Wadhwa <80780252+Satwik-Raj@users.noreply.github.com>
2025-03-27 15:25:29 +0530

    Update items_api.dart

---
+++ lib/services/items_api.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import '/models/item_model.dart';
+import 'package:http/http.dart' as http;
+
+class AuthApi{
+  static const String baseUrl = "http://127.0.0.1:5000";
+
+  // LostItem api call
+  static Future<Map<String,dynamic>> lostitem(Item item) async{
+    final response = await http.post(
+        Uri.parse("$baseUrl/lostAnItem/page2"),
+        headers: {
+          "Content-Type": "application/json"
+        },
+      body: jsonEncode(item.toJson())
+    );
+    if (response.statusCode == 200 || response.statusCode == 401) {
+      return jsonDecode(response.body);//check status codes
+    }
+
+    return {"message": "Unexpected error", "statusCode": response.statusCode};
+  }
+
+  // FoundItem api call
+  static Future<Map<String,dynamic>> founditem(Item item) async{
+    item.found();
+    final response = await http.post(
+      Uri.parse("$baseUrl/foundAnItem/page3"),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: jsonEncode(item.toJson())
+    );
+    if (response.statusCode == 200 || response.statusCode == 401) {
+      return jsonDecode(response.body);//check status codes
+    }
+
+    return {"message": "Unexpected error", "statusCode": 500};
+  }
+}
