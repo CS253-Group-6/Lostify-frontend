7f56431621517ed8d9c807ddc6bb2264d56403ad
Satwik Raj Wadhwa <80780252+Satwik-Raj@users.noreply.github.com>
2025-04-05 01:42:48 +0530

    extra property done

--- lib/pages/home_page/item_details_page.dart
+++ lib/pages/home_page/item_details_page.dart
@@ -204,7 +204,7 @@ class ItemDetailsPage extends StatelessWidget {
                 const Spacer(),
                 // Action buttons (e.g., Report, Share, Chat)
                 // if (extraProperty != null)
-                if (postOwnerId == logged_in_userId)
+                if (extraProperty != null||postOwnerId == logged_in_userId)
                 // if(true)
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
