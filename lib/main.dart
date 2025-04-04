191a9320d921c0f03fdb2c5a621c828b18e638c9
Satwik Raj Wadhwa <80780252+Satwik-Raj@users.noreply.github.com>
2025-04-05 01:40:46 +0530

    extra property done

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/home',
+      initialRoute: '/report',
       navigatorKey: navigatorKey,
       routes: {
         // auth routes
--- lib/pages/home_page/item_details_page.dart
+++ lib/pages/home_page/item_details_page.dart
@@ -203,8 +203,8 @@ class ItemDetailsPage extends StatelessWidget {
                 ),
                 const Spacer(),
                 // Action buttons (e.g., Report, Share, Chat)
-                // if (extraProperty != null)
-                if (postOwnerId == logged_in_userId)
+                // if ()
+                if (extraProperty != null||postOwnerId == logged_in_userId)
                 // if(true)
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
